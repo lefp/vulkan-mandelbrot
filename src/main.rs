212f85717c847be7a94a7e2b6e680236ed4a3afe
lefp <70862148+lefp@users.noreply.github.com>
2021-08-03 00:00:53 -0400

    Working Mandelbrot set

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,51 @@ use vulkano::{
     },
     sync::GpuFuture,
     format::{Format, ClearValue},
-    image::{ImageDimensions, StorageImage},
+    image::{
+        ImageDimensions, StorageImage,
+        view::ImageView,
+    },
+    pipeline::{ComputePipeline, ComputePipelineAbstract},
+    descriptor::descriptor_set::PersistentDescriptorSet,
+    SafeDeref,
 };
+use std::sync::Arc;
 use image::{ImageBuffer, Rgba};
+
+mod cs {
+    vulkano_shaders::shader!{
+        ty: "compute",
+        src: "
+#version 450
+
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
+
+void main() {
+    vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) /
+                            vec2(imageSize(img));
+    
+    vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
+
+    vec2 z = vec2(0.0, 0.0);
+    float i;
+    for (i = 0.0; i < 1.0; i += 0.005) {
+        z = vec2(
+            z.x * z.x - z.y * z.y + c.x,
+            z.y * z.x + z.x * z.y + c.y
+        );
+
+        if (length(z) > 4.0) {
+            break;
+        }
+    }
+
+    vec4 to_write = vec4(vec3(i), 1.0);
+    imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
+}
+"
+    }
+}
 
 fn main() {
     let instance =
@@ -32,7 +74,7 @@ fn main() {
         .find(|dev|
             dev.properties().device_name.as_ref()
             .expect("encountered unnamed device")
-            // TODO for some reason shit goes weird on the Quadro card
+            // for some reason compute shaders are jank on the Quadro
             .to_ascii_lowercase().contains("quadro")
         )
         .expect("failed to find specified device");
@@ -40,8 +82,8 @@ fn main() {
     // get a queue family that supports what we need (graphics/compute)
     let queue_family =
         physical_device.queue_families()
-        .find(|&q| q.supports_graphics())
-        .expect("failed to find a graphics queue family");
+        .find(|&q| q.supports_compute())
+        .expect("failed to find a compute queue family");
     
     // get a device and queue for the above queue family
     let (device, mut queues) =
@@ -57,11 +99,29 @@ fn main() {
     // create image
     let image = StorageImage::new(
         device.clone(),
-        // TODO should array_layers be 4? (since we use RGBA)
-        ImageDimensions::Dim2d { width: 1024, height: 1024, array_layers: 1},
+        ImageDimensions::Dim2d { width: 1024, height: 1024, array_layers: 1 },
         Format::R8G8B8A8Unorm, Some(queue.family())
     ).unwrap();
     
+    // load shader and create compute pipeline
+    let shader = cs::Shader::load(device.clone())
+        .expect("failed to create shader module");
+    let compute_pipeline = Arc::new(
+        ComputePipeline::new(
+            device.clone(), &shader.main_entry_point(), &(), None
+        )
+        .expect("failed to create compute pipeline")
+    );
+
+    // create descriptor set
+    let layout = compute_pipeline.layout().descriptor_set_layout(0).unwrap();
+    let set = Arc::new(PersistentDescriptorSet::start(layout.clone())
+        .add_image(
+            ImageView::new(image.clone()).unwrap()
+        ).unwrap()
+        .build().unwrap()
+    );
+
     // create buffer accessible by cpu (image buffers normally are not)
     let buf = CpuAccessibleBuffer::from_iter(
         device.clone(), BufferUsage::all(), false,
@@ -72,9 +132,11 @@ fn main() {
     let mut builder = AutoCommandBufferBuilder::primary(
         device.clone(), queue.family(), CommandBufferUsage::OneTimeSubmit
     ).unwrap();
-    builder.clear_color_image(
-        image.clone(), ClearValue::Float([0.0, 0.0, 1.0, 1.0])
-    ).unwrap()
+    builder
+        .dispatch(
+            [1024 / 8, 1024 / 8, 1], compute_pipeline.clone(), set.clone(), (),
+            None
+        ).unwrap()
         .copy_image_to_buffer(image.clone(), buf.clone()).unwrap();
     let command_buffer =
         builder.build().expect("failed to build command buffer");
@@ -85,7 +147,7 @@ fn main() {
         .expect("failed to execute command buffer");
     finished.then_signal_fence_and_flush().unwrap().wait(None).unwrap();
 
-    // read and save reasulting image
+    // read and save resulting image
     let buffer_content = buf.read().unwrap();
     let image = ImageBuffer::<Rgba<u8>, _>::from_raw(
         1024, 1024, &buffer_content[..]
